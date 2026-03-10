29cec9b4c216cdc4293df4405c6275c716044999
siyuanWang348 <201439910+siyuanWang348@users.noreply.github.com>
2026-03-10 13:20:00 +0800

    feat: ResumeTemplate entity (html/css/category)

---
+++ admin/src/main.ts
@@ -0,0 +1,44 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
+import App from './App.vue'
+import router from './router'
+import './styles/global.less'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(Antd)
+
+app.directive('no-autocomplete', {
+  mounted(el: HTMLElement) {
+    const inputs = el.tagName === 'INPUT' ? [el] : el.querySelectorAll('input')
+    inputs.forEach((input: Element) => {
+      input.setAttribute('autocomplete', 'off')
+      input.setAttribute('autocorrect', 'off')
+      input.setAttribute('autocapitalize', 'off')
+    })
+  },
+})
+
+app.mixin({
+  mounted() {
+    const el = (this as any).$el
+    if (el && el.querySelectorAll) {
+      const inputs = el.querySelectorAll('input, textarea')
+      inputs.forEach((input: Element) => {
+        if (!input.getAttribute('autocomplete')) {
+          input.setAttribute('autocomplete', 'new-password')
+        }
+      })
+      const forms = el.querySelectorAll('form')
+      forms.forEach((form: Element) => {
+        form.setAttribute('autocomplete', 'off')
+      })
+    }
+  },
+})
+
+app.mount('#app')
