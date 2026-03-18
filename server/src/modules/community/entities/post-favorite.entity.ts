babe29f332c7f050d6199b8dc074cbde6d767631
TJbubble <205479911+TJbubble@users.noreply.github.com>
2026-03-18 15:28:00 +0800

    fix: route guard (requireAuth)

---
+++ server/src/modules/community/entities/post-favorite.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  Unique,
+} from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+import { Post } from './post.entity';
+
+@Entity('post_favorites')
+@Unique(['userId', 'postId'])
+export class PostFavorite {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ comment: '用户ID' })
+  userId: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column({ comment: '帖子ID' })
+  postId: number;
+
+  @ManyToOne(() => Post)
+  @JoinColumn({ name: 'postId' })
+  post: Post;
+
+  @CreateDateColumn({ comment: '收藏时间' })
+  createdAt: Date;
+}
